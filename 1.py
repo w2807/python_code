c593efad22d2108602cf6aaecfb089904b353c54
w2807 <w2807090881@outlook.com>
2022-12-18 18:18:39 +0800

    git commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+print(r'this line is a test\n',end='')
+sentence='hello world!'
+print('\n')
+print(sentence*2+'\ngoodbye world')
+a=True
+if a :
+    print('it is true',end='')
+    print('\n')
+else :
+    print('not true')
